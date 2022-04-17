98692ac92c5ec74113d55f62c699062838464c43
melikarameli <melikahatunic2001@gmail.com>
2022-04-17 22:00:06 +0200

    Added ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+let arena = new Map();
+arena.author = ("Gigi");
+arena.publisher = ("Mistral");
+arena.numberofPages = (28);
+arena.year = (2021);
+console.log(arena);
+// Only change code above this line
+for (let value of arena) console.log(value);
+module.exports = arena;
