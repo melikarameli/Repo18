276cf605ca4b6d5d3cb9bb85878e5b9d7417c25b
melikarameli <melikahatunic2001@gmail.com>
2022-04-17 20:51:47 +0200

    Added ex01

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,10 +1,17 @@
 let myArr = [];
 // Only change code below this line
-function multiplication(x) {
-    for (var x; x * 2; x++) {
-        yield x;
+function* multiplication(x) {
+    var y = x + 1;
+    for (var i = 1; i <= y; i++ ) {
+        x = x * 2;
         myArr.push(x);
+        yield "I am " + i + " yield";
     }
 }
+const multi = multiplication(3); 
+console.log(multi.next());
+console.log(multi.next());
+console.log(multi.next());
+console.log(myArr);
 // Only change code above this line
 module.exports = multiplication;
